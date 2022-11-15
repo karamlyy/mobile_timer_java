7570ff45bfde1ba06fb3608aa76362deaa953b2b
Karam Afandi <77574018+karamlyy@users.noreply.github.com>
2022-11-15 23:20:07 +0400

    smth

---
+++ app/src/main/java/com/karamlyy/runnablehandler/MainActivity.java
@@ -0,0 +1,68 @@
+package com.karamlyy.runnablehandler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    int number;
+    Runnable runnable;
+    Handler handler;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        number = 0;
+    }
+
+    public void start(View view){
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                textView.setText("Time: " + number);
+                number++;
+                textView.setText("Time: " + number);
+                handler.postDelayed(runnable, 1000);
+            }
+        };
+        handler.post(runnable);
+        button.setEnabled(false);
+    }
+
+    public void stop(View view){
+        button.setEnabled(true);
+        handler.removeCallbacks(runnable);
+        Toast.makeText(this, number + " second ", Toast.LENGTH_LONG).show();
+        number = 0;
+        textView.setText("Time: " + number);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
